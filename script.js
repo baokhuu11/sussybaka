994721cfe7258cb0a467e0b88411a35e916946ba
baokhuu11 <baokhuu11234@gmail.com>
2025-02-21 14:09:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="sussybaka" && p=="6969 "
+      {
+    window.open("index.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
