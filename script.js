490850ee959c49112824e8dd1d39ca20e39d0006
baokhuu11 <baokhuu11234@gmail.com>
2025-03-26 11:53:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="sussybaka" && p=="22")
+  {
+    window.open("login.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
+
+
+  
